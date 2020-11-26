a201b3cdcddab8a909047a6bb3c4925edf99dc25
alchemistmatt <matthew.monroe@pnnl.gov>
2020-11-25 16:09:19 -0800

    Use new filename

--- Data/MSGFPlus_Example/ProcessData.bat
+++ Data/MSGFPlus_Example/ProcessData.bat
@@ -15,7 +15,7 @@ echo Procesing with %ExePath%
 echo.
 
 echo Example 1, process a PHRP _syn.txt file
-%ExePath% QC_Shew_16_01-15f_13_19Nov16_Tiger_16-02-15_msgfdb_syn.txt
+%ExePath% QC_Shew_16_01-15f_13_19Nov16_Tiger_16-02-15_msgfplus_syn.txt
 
 echo Example 2, process a .tsv file created by MzidToTsvConverter using an MSGF+ .mzid file
 %ExePath% QC_Shew_16_01-15f_13_19Nov16_Tiger_16-02-15_msgfplus.tsv
